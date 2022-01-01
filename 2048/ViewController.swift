e275a6ece96fa6e3bda3cadf63992a5b6af23dcd
shubhamydv932 <83470786+shubhamydv932@users.noreply.github.com>
2022-01-01 22:43:35 +0530

    added score updater and autoplayer

--- 2048/ViewController.swift
+++ 2048/ViewController.swift
@@ -11,45 +11,56 @@ class ViewController: UIViewController {
 
     @IBOutlet var holder : UIView!
     
+    var endGame : Bool = true
+    
+    
+    var matrix : [[Int]] = [[0,0,0,0] , [0,0,0,0] , [0,0,0,0] , [0,0,0,0]]
+    var updatedScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(rightSwipe))
         swipeRight.direction = .right
         self.view.addGestureRecognizer(swipeRight)
 
+    
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(leftSwipe))
         swipeLeft.direction = .left
         self.view.addGestureRecognizer(swipeLeft)
-        
+
+    
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(upSwipe))
         swipeUp.direction = .up
         self.view.addGestureRecognizer(swipeUp)
-        
+
+    
         let swipeBottom = UISwipeGestureRecognizer(target: self, action: #selector(bottomSwipe))
         swipeBottom.direction = .down
         self.view.addGestureRecognizer(swipeBottom)
-
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         holder.backgroundColor = .white
         gameScreen()
+        
 
     }
+   
     
-    var matrix : [[Int]] = [[0,0,0,0] , [0,0,0,0] , [0,0,0,0] , [0,0,0,0]]
-    let updatedScore = ""
-    
-    
-    
-    
-    
-    private func gameScreen(){
+    private  func gameScreen(){
         newNumber()
         let boxSize : CGFloat = holder.frame.size.width / 4;
+        
+        
+        let autoGestureButton = UIButton(frame: CGRect(x: 10, y: 10, width: boxSize + 50, height: boxSize - 30))
+        autoGestureButton.setTitle("AUTOPLAY", for: .normal)
+        autoGestureButton.setTitleColor(.green, for: .normal)
+        autoGestureButton.backgroundColor = .lightGray
+        autoGestureButton.addTarget(self, action: #selector(autoGestures(_:)), for: .touchUpInside)
+        holder.addSubview(autoGestureButton)
         
         for i in 0..<4{
             for j in 0..<4{
@@ -90,22 +101,12 @@ class ViewController: UIViewController {
         }
     }
     
-   // func to find number of non zero elements in a row
-    @objc func numRow(array : [Int]) -> Int{
-        var count = 0
-        for i in 0..<4{
-            if array[i] != 0{
-                count += 1
-            }
-        }
-        return count
-    }
     
     // for left swipe gesture
     
     @objc func leftSwipe(){
-        var counter = 0
         for i in 0..<4{
+            var counter = 0
             for j in 0..<4{
                 if matrix[i][j] != 0{
                     matrix[i][counter] = matrix[i][j]
@@ -116,35 +117,29 @@ class ViewController: UIViewController {
         
         
         for i in 0..<4{
-            var numberOfElements = numRow(array: matrix[i])
-            if numberOfElements == 2{
-                for j in 0..<2{
-                    if matrix[i][j] == matrix[i][j+1]{
-                        matrix[i][j] = matrix[i][j] + matrix[i][j+1]
-                        matrix[i][j+1] = 0
+            for j in 0..<3{
+                if matrix[i][j] == matrix[i][j+1]{
+                    matrix[i][j] = 2 * matrix[i][j]
+                    updatedScore = updatedScore + matrix[i][j]
+                    for k in j+1..<4{
+                        if k == 3{
+                            matrix[i][k] = 0
+                        }
+                        else{
+                            matrix[i][k] = matrix[i][k+1]
+                        }
                     }
                 }
-            }
-            else if numberOfElements == 3{
-                for j in 0..<2{
-                    if matrix[i][j] == matrix[i][j+1]{
-                        matrix[i][j] = matrix[i][j] + matrix[i][j+1]
-                        matrix[i][j+1] = matrix[i][j+2]
-                        matrix[i][j+2] = 0
-                    }
-                }
-            }
-            else if numberOfElements == 4{
-                for j in 0..<
             }
         }
+        gameScreen()
     }
     
     // for right swipe gesture
     
     @objc func rightSwipe(){
-        var counter = 3
         for i in 0..<4{
+            var counter = 3
             for j in stride(from: 3, through: 0, by: -1){
                 if matrix[i][j] != 0{
                     matrix[i][counter] = matrix[i][j]
@@ -153,22 +148,31 @@ class ViewController: UIViewController {
             }
         }
         
-        
         for i in 0..<4{
-            for j in stride(from: 3, to: 0, by: -1){
+            for j in stride(from: 3, through: 1, by: -1){
                 if matrix[i][j] == matrix[i][j-1]{
-                    matrix[i][j] = matrix[i][j] + matrix[i][j-1]
+                    matrix[i][j] = 2 * matrix[i][j]
+                    updatedScore = updatedScore + matrix[i][j]
+                    for k in stride(from: j-1, through: 0, by: -1){
+                        if k == 0{
+                            matrix[i][k] = 0
+                        }
+                        else{
+                            matrix[i][k] = matrix[i][k-1]
+                        }
+                    }
                 }
             }
         }
         
+        gameScreen()
     }
     
     // for up swipe gesture
     
-    @objc func upSwipe() -> [[Int]]{
-        var counter = 0
+    @objc func upSwipe(){
         for j in 0..<4{
+            var counter = 0
             for i in 0..<4{
                 if matrix[i][j] != 0{
                     matrix[counter][j] = matrix[i][j]
@@ -179,21 +183,30 @@ class ViewController: UIViewController {
         
         
         for j in 0..<4{
-            for i in 0..<4{
+            for i in 0..<3{
                 if matrix[i][j] == matrix[i+1][j]{
-                    matrix[i][j] = matrix[i][j] + matrix[i+1][j]
+                    matrix[i][j] = 2 * matrix[i][j]
+                    updatedScore = updatedScore + matrix[i][j]
+                    for k in i+1..<4{
+                        if k == 3{
+                            matrix[k][j] = 0
+                        }
+                        else{
+                            matrix[k][j] = matrix[k+1][j]
+                        }
+                    }
                 }
             }
         }
         
-        return matrix
+        gameScreen()
     }
     
     // for bottom swipe
     
     @objc func bottomSwipe(){
-        var counter = 3
         for j in 0..<4{
+            var counter = 3
             for i in stride(from: 3, through: 0, by: -1){
                 if matrix[i][j] != 0{
                     matrix[counter][j] = matrix[i][j]
@@ -202,22 +215,102 @@ class ViewController: UIViewController {
             }
         }
         
+        for j in 0..<4{
+            for i in stride(from: 3, through: 1, by: -1){
+                if matrix[i][j] == matrix[i-1][j]{
+                    matrix[i][j] = 2 * matrix[i][j]
+                    updatedScore = updatedScore + matrix[i][j]
+                    for k in stride(from: i-1, through: 0, by: -1){
+                        if k == 0{
+                            matrix[k][j] = 0
+                        }
+                        else{
+                            matrix[k][j] = matrix[k-1][j]
+                        }
+                    }
+                }
+            }
+        }
+
+        gameScreen()
+    }
+    
+    @objc func autoGestures(_ sender : UIButton){
+        DispatchQueue.main.async {
+            self.detectEndGame()
+            while  self.endGame == false{
+                let autoGesture = Int.random(in: 0...3)
+                switch autoGesture{
+                    case 0:
+                        self.leftSwipe()
+                    case 1:
+                        self.rightSwipe()
+                    case 2:
+                        self.upSwipe()
+                    case 3:
+                        self.bottomSwipe()
+                    default:
+                        self.gameEnded()
+                        
+                }
+            }
+        }
+
+    }
+    
+    
+    @objc func gameEnded(){
+        detectEndGame()
+        if endGame == true{
+            view.backgroundColor = .black
+        }
+    }
+    
+    
+    private func detectEndGame(){
+        for i in 0...3{
+            for j in 0...2{
+                if matrix[i][j] == matrix[i][j+1]{
+                    endGame = false
+                }
+            }
+            
+        }
         
-//        for j in 0..<4{
-//            for i in 3...0{
-//                if matrix
-//            }
-//        }
+        for j in 0...3{
+            for i in 0...2{
+                if matrix[i][j] == matrix[i+1][j]{
+                    endGame = false
+                }
+            }
+            
+        }
         
+        for i in 0...3{
+            for k in 0...3{
+                if matrix[i][k] == 0{
+                    endGame = false
+                }
+            }
+        }
         
     }
     
-    // to update the score
-    
-//    @objc func updateScore() -> Int {
-//
+//    private func updateScore() -> Int{
+//        for i in 0...3{
+//            for j in 0...3{
+//                if matrix[i][j] > 2{
+//                    updatedScore = updatedScore + matrix[i][j]
+//                }
+//            }
+//        }
+//        return updatedScore
 //    }
 
 
 }
 
+
+// end game detect. display game over
+// auto play button
+// score update
